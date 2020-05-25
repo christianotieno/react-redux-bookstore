e066f1d47a9dc122da69b5ae37110875f474149e
Christian Otieno <oduorchristian@gmail.com>
2020-05-26 00:18:26 +0300

    Rename App component

---
+++ src/components/BooksList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const BooksList = () => (
+  <div>
+    <table>
+      <tr>
+        <th>Book ID</th>
+        <th>title</th>
+        <th>category</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>book title</td>
+        <td>category</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>another book title</td>
+        <td>another category</td>
+      </tr>
+    </table>
+  </div>
+);
+
+
+export default BooksList;
