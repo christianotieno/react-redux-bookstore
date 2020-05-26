7f33809b8fe6d0c2cdebd44f72b6254409ca3d59
Christian Otieno <oduorchristian@gmail.com>
2020-05-26 22:18:38 +0300

    Implement remove book functionality

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Book = ({ book }) => (
-  <tr>
-    <td>{book.id}</td>
-    <td>{book.title}</td>
-    <td>{book.category}</td>
-  </tr>
-);
+const Book = ({ book, deleteBook }) => {
+  const handleRemoveBook = () => {
+    deleteBook(book);
+  };
+  return (
+    <tr>
+      <td>{book.id}</td>
+      <td>{book.title}</td>
+      <td>{book.category}</td>
+      <td>
+        <button
+          type="button"
+          onClick={handleRemoveBook}
+        >
+          Remove Book
+        </button>
+
+      </td>
+    </tr>
+  );
+};
 
 Book.propTypes = {
   book: PropTypes.shape({
@@ -15,6 +29,7 @@ Book.propTypes = {
     title: PropTypes.string.isRequired,
     category: PropTypes.string.isRequired,
   }).isRequired,
+  deleteBook: PropTypes.func.isRequired,
 };
 
 export default Book;
