3f917365d479b90cd82621d2bb0a415bf127813a
christianotieno <oduorchristian@gmail.com>
2020-06-20 03:02:13 +0300

    Fix issue with remove book form database

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@ const CREATE_BOOK = book => ({ type: 'CREATE_BOOK', payload: book });
 
 const filterCategory = filter => ({ type: 'CHANGE_FILTER', payload: filter });
 
-const REMOVE_BOOK = book => ({ type: 'REMOVE_BOOK', payload: book.id });
+const REMOVE_BOOK = id => ({ type: 'REMOVE_BOOK', payload: id });
 
 const GET_BOOKS = books => ({ type: 'GET_BOOKS', payload: books });
 
--- src/actions/removeBook.js
+++ src/actions/removeBook.js
@@ -2,16 +2,18 @@ import axios from 'axios';
 import { REMOVE_BOOK } from './index';
 /* eslint-disable no-debugger, no-console */
 
-const removeBook = id => dispatch => {
-  dispatch(REMOVE_BOOK(id));
+const removeBook = book => dispatch => {
+  dispatch(REMOVE_BOOK(book));
   axios.delete(
-    `https://immense-taiga-94124.herokuapp.com/books/${id}`,
+    `https://immense-taiga-94124.herokuapp.com/books/${book.id}`,
   ).then(response => {
     if (response.error) {
       console.log(response.error);
+      console.log(book.id);
     }
   }).catch(error => {
     console.log(error);
+    console.log(book.id);
   });
 };
 
